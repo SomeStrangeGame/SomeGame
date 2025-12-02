6cdbd53fad6a4d337a9a357ed555e13ae57e2419
iantonishin <iantonishin@yandex-team.ru>
2025-12-02 19:36:55 +0300

    add rotation logic

--- Game/Assets/Game/Character/TestCharacterMover.cs
+++ Game/Assets/Game/Character/TestCharacterMover.cs
@@ -4,9 +4,12 @@ using UnityEngine.AI;
 public class TestCharacterMover : MonoBehaviour
 {
     [SerializeField] private Transform _target;
+    [SerializeField] private Transform _lookAtTarget;
 
     private Animator _anim;
     private NavMeshAgent _navAgent;
+
+    private float _rot;
 
     private void OnEnable()
     {
@@ -18,8 +21,34 @@ public class TestCharacterMover : MonoBehaviour
     {
         _navAgent.SetDestination(_target.position);
         var vel = _navAgent.velocity;
-        _anim.SetFloat("Vert", Mathf.Clamp(transform.InverseTransformDirection(vel).z, -2f, 2f));
-        _anim.SetFloat("Hor", Mathf.Clamp(transform.InverseTransformDirection(vel).x, -2f, 2f));
+        var clampValue = 2f;
+        var vertValue = Mathf.Clamp(transform.InverseTransformDirection(vel).z, -clampValue, clampValue);
+        var horValue = Mathf.Clamp(transform.InverseTransformDirection(vel).x, -clampValue, clampValue);
+        _anim.SetFloat("Vert", vertValue);
+        _anim.SetFloat("Hor", horValue);
+
+        _anim.applyRootMotion = Mathf.Abs(vertValue) + Mathf.Abs(horValue) < 0.1f;
+
+        var targetDotForward = GetDot(_anim.transform, _lookAtTarget, Vector3.forward);
+        var targetDotRight = GetDot(_anim.transform, _lookAtTarget, Vector3.right);
+        targetDotRight = targetDotRight > 0f ? 1f : -1f;
+
+        _rot = Mathf.Lerp(_rot, targetDotRight, Time.deltaTime * 5f);
+
+        var isRot = targetDotForward < 0f && _anim.applyRootMotion;
+        _anim.SetBool("IsRot", isRot);
+        _anim.SetFloat("Rot", _rot);
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        _anim.SetLookAtPosition(_lookAtTarget.position);
+        _anim.SetLookAtWeight(1f, 0.5f, 0.7f, 0.9f, 0.5f);
+    }
+
+    private float GetDot(Transform origin, Transform target, Vector3 axis)
+    {
+        return Vector3.Dot(origin.TransformDirection(axis).normalized, (target.position - origin.position).normalized);
     }
 
 }
