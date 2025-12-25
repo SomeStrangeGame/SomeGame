feaa61d76bda7c591bd88032904d22b564df4698
iantonishin <iantonishin@yandex-team.ru>
2025-12-25 12:49:25 +0300

    small update

---
+++ Game/Assets/Game/Chapter_ScreenAndBattle/View/FPSCounter.cs
@@ -0,0 +1,42 @@
+using TMPro;
+using UnityEngine;
+
+namespace Game.Chapter_ScreenAndBattle.View
+{
+    public class FPSCounter : MonoBehaviour
+    {
+        [SerializeField] [Range(0f, 1f)] private float _expSmoothingFactor = 0.9f;
+        [SerializeField] private float _refreshFrequency = 0.4f;
+
+        private float _timeSinceUpdate = 0f;
+        private float _averageFps = 1f;
+
+        private TMP_Text _text;
+        private TMP_Text Text
+        {
+            get
+            {
+                if (_text == null) _text = GetComponentInChildren<TMP_Text>(true);
+                return _text;
+            }
+        }
+
+        private void Update()
+        {
+            // Exponentially weighted moving average (EWMA)
+            _averageFps = _expSmoothingFactor * _averageFps + (1f - _expSmoothingFactor) * 1f / Time.unscaledDeltaTime;
+
+            if (_timeSinceUpdate < _refreshFrequency)
+            {
+                _timeSinceUpdate += Time.deltaTime;
+                return;
+            }
+
+            var fps = Mathf.RoundToInt(_averageFps);
+            Text.text = fps.ToString();
+
+            _timeSinceUpdate = 0f;
+        }
+    }
+}
+
