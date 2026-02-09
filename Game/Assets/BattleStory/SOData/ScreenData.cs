4a16d9bfa620c2e166349371adb92aee61709eac
iantonishin <iantonishin@yandex-team.ru>
2026-02-09 16:23:52 +0300

    add voices test

--- Game/Assets/BattleStory/SOData/ScreenData.cs
+++ Game/Assets/BattleStory/SOData/ScreenData.cs
@@ -8,10 +8,12 @@ namespace BattleStory.SOData
         [SerializeField] private string _textAssetName;
         [SerializeField] private StoryScreenData _screenBundle;
         [SerializeField] private BundleData _backgroundBundle;
+        [SerializeField] private string _voiceAssetsName;
 
         public string TextAssetName => _textAssetName;
         public StoryScreenData ScreenBundle => _screenBundle;
         public BundleData BackgroundBundle => _backgroundBundle;
+        public string VoiceAssetsName => _voiceAssetsName;
     }
 }
 
