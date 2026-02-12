dacc02e0c2033ebd218103ea0c084ba07bca4394
iantonishin <iantonishin@yandex-team.ru>
2026-02-12 11:17:58 +0300

    bubble show fix

--- Game/Assets/Novels/Bubble/View/Screen.cs
+++ Game/Assets/Novels/Bubble/View/Screen.cs
@@ -11,27 +11,60 @@ namespace Novels.Bubble.View
         [SerializeField] private Text _text;
         [SerializeField] private Button _buttonPrefab;
         [SerializeField] private Button _backgroundButton;
+        [SerializeField] private float _showHideDuration;
+        [SerializeField] private CanvasGroup _canvasGroup;
 
         private readonly Dictionary<int, Button> _buttons = new();
 
         public void ShowImmediate()
         {
-            gameObject.SetActive(true);
-        }
-
-        public void HideImmediate()
-        {
-            gameObject.SetActive(false);
+            _canvasGroup.alpha = 1f;
+            _canvasGroup.gameObject.SetActive(true);
         }
 
         public async UniTask Show()
         {
-            gameObject.SetActive(true);
+            _canvasGroup.alpha = 0f;
+            _canvasGroup.gameObject.SetActive(true);
+
+            var delayMs = 50;
+            var deltaTime = delayMs / 1000f;
+
+            var timer = _showHideDuration;
+            while (timer >= 0f)
+            {
+                _canvasGroup.alpha = 1f - (timer / _showHideDuration);
+                timer -= deltaTime;
+                await UniTask.Delay(delayMs, true);
+            }
+
+            _canvasGroup.alpha = 1f;
+        }
+
+        public void HideImmediate()
+        {
+            _canvasGroup.alpha = 0f;
+            _canvasGroup.gameObject.SetActive(false);
         }
 
         public async UniTask Hide()
         {
-            gameObject.SetActive(false);
+            _canvasGroup.alpha = 1f;
+            _canvasGroup.gameObject.SetActive(true);
+
+            var delayMs = 50;
+            var deltaTime = delayMs / 1000f;
+
+            var timer = _showHideDuration;
+            while (timer >= 0f)
+            {
+                _canvasGroup.alpha = timer / _showHideDuration;
+                timer -= deltaTime;
+                await UniTask.Delay(delayMs, true);
+            }
+
+            _canvasGroup.alpha = 0f;
+            _canvasGroup.gameObject.SetActive(false);
         }
 
         public void SetText(string text)
